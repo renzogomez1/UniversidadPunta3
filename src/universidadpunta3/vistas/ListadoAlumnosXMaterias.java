2185cd8e8a3f6b963bc96730532b095f47d8e580
Emiliano <Emiliano@Emiliano>
2021-05-20 19:52:28 -0300

    Cambio de nombre de paquete

--- src/universidadpunta3/vistas/ListadoAlumnosXMaterias.java
+++ src/universidadpunta3/vistas/ListadoAlumnosXMaterias.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vistas;
+package universidadpunta3.vistas;
 
 import java.util.ArrayList;
 import java.util.logging.Level;
